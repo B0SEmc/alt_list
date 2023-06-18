6de889037706a0c25771224a3f232eae0adc4d4a
Thibaut <thibaut.musslin@gmail.com>
2023-06-18 18:00:25 +0200

    Fixed add

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,9 @@ fn main() {
             match ign {
                 Some(ign) => {
                     let mut list = fs::read_to_string("list.txt").expect("alt list couldn't be found.");
-                    list.push_str(&format!("{} r", ign.trim()));
+                    // remove empty lines from the list
+                    list = list.replace("\n\n", "\n");
+                    list.push_str(&format!("\n{} r", ign.trim()));
                     fs::write("list.txt", list).expect("Unable to write file");
                     // if successful, print this: ign in red and "added to list" in green
                     println!("{} {}",  ign.red(), "added to list".green());
@@ -58,14 +60,12 @@ fn main() {
             match ign {
                 Some(ign) => {
                     let mut list = fs::read_to_string("list.txt").expect("alt list couldn't be found.");
-                    // detect if the ign is in the list or not
                     if !list.contains(&format!("{} r", ign.trim())) || !list.contains(&ign.trim().to_string()) {
                         println!("Error: {} is not in the list.", ign);
                         return;
                     }
                     // remove the ign from the list
-                    list = list.replace(&format!("{} r", ign.trim()), "");
-                    list = list.replace(&ign.trim().to_string(), "");
+                    list = list.replace(&format!("{} r\n", ign.trim()), "");
                     // also remove the empty line
                     list = list.replace("\n\n", "\n");
                     fs::write("list.txt", list).expect("Unable to write file");
